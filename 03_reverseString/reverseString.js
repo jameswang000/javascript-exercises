2203c1f8ff2c17b81f5e6656b7c834072b8fd6f7
James Wang <jmaeswnag@gmail.com>
2024-05-16 15:39:50 -0400

    Finished a few exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let n = s.length;
+    let res = "";
+    for(let i = n - 1 ; i >= 0 ; i--) {
+        res += s.slice(i, i+1);
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(l, r) {
+    if (typeof l != "number" || typeof r != "number" ||
+        l < 0 || r < 0) {
+        return "ERROR";
+    }
 
+    let start = undefined;
+    let end = undefined;
+    if (l < r) {
+        start = l;
+        end = r;
+    }
+    else {
+        start = r;
+        end = l;
+    }
+
+    let res = 0;
+    for(let i = start ; i <= end ; i++) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
